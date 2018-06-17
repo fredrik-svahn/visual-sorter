3abf13b80a7c429ffcb61e5b90c8811863532d59
frsvahn <frsvahn@kth.se>
2018-06-17 13:41:10 +0200

    Added test for sorter
    The test checks that sorting an empty array should produce that same array.

--- src/Sorter.java
+++ src/Sorter.java
@@ -1,2 +1,9 @@
 public class Sorter {
+    /**
+     * Should sort an array of integer in ascending order.
+     * @param array the array to be sorted.
+     */
+    public void sort(int[] array) {
+
+    }
 }
--- test/SorterTest.java
+++ test/SorterTest.java
@@ -1,5 +1,32 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.*;
 
 public class SorterTest {
+    private Sorter sorter;
+    private int[] emptyArray;
 
+    @Before
+    public void setup() {
+        sorter = new Sorter();
+
+        emptyArray = new int[0];
+    }
+
+    @Test
+    public void sortingEmptyArrayShouldNotDoAnything() {
+        int[] copy = copyArray(emptyArray);
+
+        sorter.sort(emptyArray);
+
+        assertThat(emptyArray, equalTo(copy));
+    }
+
+    private int[] copyArray(int[] target) {
+        return Arrays.copyOf(target, target.length);
+    }
 }
